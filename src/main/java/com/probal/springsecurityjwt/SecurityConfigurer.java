fc82538a98fddbfafc7a6cc103231c679b25d946
Probal Das <49045586+probaldas@users.noreply.github.com>
2021-07-03 21:31:54 +0530

    Create request response class for /authenticate endpoint and logic for the same in HelloResource. Added AuthenticationManager bean and disabled security for /authenticate in SecurityConfigurer.

--- src/main/java/com/probal/springsecurityjwt/SecurityConfigurer.java
+++ src/main/java/com/probal/springsecurityjwt/SecurityConfigurer.java
@@ -3,7 +3,9 @@ package com.probal.springsecurityjwt;
 import com.probal.springsecurityjwt.service.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,6 +20,19 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/authenticate").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Bean
