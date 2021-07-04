712928fad1a23592fbe01c9e14be38cdecd113f9
Probal Das <49045586+probaldas@users.noreply.github.com>
2021-07-04 08:49:10 +0530

    Renamed HelloResource to AuthenticateResource

--- src/main/java/com/probal/springsecurityjwt/AuthenticateResource.java
+++ src/main/java/com/probal/springsecurityjwt/AuthenticateResource.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloResource {
+public class AuthenticateResource {
 
     @Autowired
     private AuthenticationManager authenticationManager;
